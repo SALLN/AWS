3903d9a90110bd588500b156c7904b8125b99169
SALLN <stevenllerena@hotmail.com>
2016-03-06 14:59:14 -0500

    ExtraerDb terminado 100%

--- EXTRAERDB.php
+++ EXTRAERDB.php
@@ -2,7 +2,7 @@
 
 $con_aws = mysql_connect("localhost","root","ticoll") or die("problemas al conectar con mysql");
 mysql_select_db("diseno",$con_aws) or die("Problemas la seleccionar con database");
-$consulta=mysql_query("SELECT LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO FROM coordenadas where id=1",$con_aws);
+$consulta=mysql_query("SELECT LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO FROM coordenadas",$con_aws);
 
 $con_local = mysql_connect("186.82.161.239:3307","STEVEN","ticoll") or die("problemas al conectar con mysql");
 mysql_select_db("diseno",$con_local) or die("Problemas la seleccionar con database");
