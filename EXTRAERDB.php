5f88a1b11cd1d4e71b3f897bae6b6c3cf9d823e4
SALLN <stevenllerena@hotmail.com>
2016-03-06 14:57:30 -0500

    ExtraerDb listo

--- EXTRAERDB.php
+++ EXTRAERDB.php
@@ -2,10 +2,10 @@
 
 $con_aws = mysql_connect("localhost","root","ticoll") or die("problemas al conectar con mysql");
 mysql_select_db("diseno",$con_aws) or die("Problemas la seleccionar con database");
-$consulta=mysql_query("SELECT LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO FROM coordenadas",$con_aws);
+$consulta=mysql_query("SELECT LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO FROM coordenadas where id=1",$con_aws);
 
-//$con_local = mysql_connect("186.82.161.239:3307","STEVEN","ticoll") or die("problemas al conectar con mysql");
-//mysql_select_db("diseno",$con_local) or die("Problemas la seleccionar con database");
+$con_local = mysql_connect("186.82.161.239:3307","STEVEN","ticoll") or die("problemas al conectar con mysql");
+mysql_select_db("diseno",$con_local) or die("Problemas la seleccionar con database");
 
 while($tablaDB=mysql_fetch_array($consulta)){
 
@@ -14,10 +14,8 @@ $longitud=$tablaDB['LONGITUD'];
 $fecha=$tablaDB['FECHA_HORA'];
 $id_vehiculo=$tablaDB['ID_VEHICULO'];
 
-echo $latitud;
-echo "<br>";
-//mysql_query("INSERT INTO coordenadas2 (LATITUD,LONGITUD,HORA,FECHA,ID_VEHICULO) 
-//VALUES ('$latitud','$longitud', '$fecha','$id_vehiculo')",$con_local);
+mysql_query("INSERT INTO coordenadas2 (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO) 
+	VALUES ('$latitud','$longitud', '$fecha','$id_vehiculo')",$con_local);
 
 }
 ?>
