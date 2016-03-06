ad3d20c9d069341d9c144dca06c9ed3431242629
SALLN <stevenllerena@hotmail.com>
2016-03-05 19:27:43 -0500

    Actualizando todo
    funciona calendario, time, historico

--- ConsultaDbHistorico.php
+++ ConsultaDbHistorico.php
@@ -5,18 +5,20 @@ include("conexionmysql_aws.php");
 $con=mysql_connect($host,$user,$pw) or die ("Problemas al conectar");
 mysql_select_db($db,$con) or die ("Problema al conectar con la DB");
 
-$registro=mysql_query("SELECT LATITUD,LONGITUD,HORA,FECHA FROM coordenadas 
-						where fecha between  '$_POST[FechaInicio]' and '$_POST[FechaFinal]' 
-						and hora between '$_POST[HoraInicio]' and '$_POST[HoraFinal]' 
- 						order by  id asc") or die("Problemas en consulta: ".mysql_error());
+$fecha_start=$_POST['FechaInicio']." ".$_POST['HoraInicio'];
+$fecha_end=$_POST['FechaFinal']." ".$_POST['HoraFinal'];
+
+$registro=mysql_query("SELECT LATITUD,LONGITUD,FECHA_HORA FROM coordenadas 
+						where FECHA_HORA between  '$fecha_start' and '$fecha_end'						
+ 						order by  ID asc") or die("Problemas en consulta: ".mysql_error());
 
 
 while($reg=mysql_fetch_array($registro)){  
 
 $lati2[]=$reg['LATITUD']; 	
 $longi2[]=$reg['LONGITUD'];
-$fechat2[]=$reg['FECHA'];
-$horat2[]=$reg['HORA'];
+$fechat2[]=substr($reg['FECHA_HORA'],0,10);
+$horat2[]=substr($reg['FECHA_HORA'],11,18);
 
 }
 
