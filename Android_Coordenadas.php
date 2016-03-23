ba7ffc795f142d8b68a2e3f2fa12ed44b44512d2
SALLN <stevenllerena@hotmail.com>
2016-03-23 03:26:20 -0500

    Archivo android corregido mysql

--- Android_Coordenadas.php
+++ Android_Coordenadas.php
@@ -1,6 +1,6 @@
 <?php
 
-include("MySQL/conexionmysql_aws.php");
+include("MySQL/ConexionMySQL.php");
 
 $consulta=mysql_query("INSERT INTO $_POST[Usuario] (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO) VALUES('$_POST[Latitud_gps]','$_POST[Longitud_gps]','$_POST[Fecha_Hora_gps]','$_POST[Id_vehiculo]')");
 
