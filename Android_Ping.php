8a0d7bf301e6df0dae6941bd5bfdcdca1721061e
SALLN <stevenllerena@hotmail.com>
2016-04-16 12:21:01 -0500

    1.0

--- Android_Ping.php
+++ Android_Ping.php
@@ -6,8 +6,9 @@ $Fechas=$_POST['Fechas'];
 $Servidores=$_POST['Servidores'];
 $Retardos=$_POST['Retardos'];
 $Jitters=$_POST['Jitters'];
+$integerIDs = json_decode('[' . $Fechas . ']', true);
 echo "Si responde";
-echo $Fechas[0];
+echo $integerIDs[0];
 
 //$consulta=mysql_query("INSERT INTO Pings (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO,FECHA_HORA_SERV) VALUES('$_POST[Latitud_gps]','$_POST[Longitud_gps]','$_POST[Fecha_Hora_gps]','$_POST[Id_vehiculo]','$fecha_servidor')");
 
