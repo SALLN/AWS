0ad9027c79de6add1a26fb77cb057d422a55ea90
SALLN <stevenllerena@hotmail.com>
2016-04-16 12:05:24 -0500

    Pingg

--- Android_Ping.php
+++ Android_Ping.php
@@ -8,6 +8,7 @@ $Retardos=$_POST['Retardos'];
 $Jitters=$_POST['Jitters'];
 echo "Si responde";
 echo $Fechas[0];
+
 //$consulta=mysql_query("INSERT INTO Pings (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO,FECHA_HORA_SERV) VALUES('$_POST[Latitud_gps]','$_POST[Longitud_gps]','$_POST[Fecha_Hora_gps]','$_POST[Id_vehiculo]','$fecha_servidor')");
 
 //mysql_free_result($consulta);
