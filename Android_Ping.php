44587ba17577e6c818c55ca08bc00386b9afc4da
SALLN <stevenllerena@hotmail.com>
2016-04-17 01:39:15 -0500

    1.9

--- Android_Ping.php
+++ Android_Ping.php
@@ -2,30 +2,18 @@
 
 include("MySQL/ConexionMySQL.php");
 
-$Fechas=$_POST['Fechas'];
-$Servidores=$_POST['Servidores'];
-$Retardos=$_POST['Retardos'];
-$Jitters=$_POST['Jitters'];
-
-
-$Fechas=split('&',$Fechas);
-$Servidores=split('&',$Servidores);
-$Retardos=split('&',$Retardos);
-$Jitters=split('&',$Jitters);
-
-echo "si responde";
-$Longitud=count($Fechas);
-
+$Fechas=split('&',$_POST['Fechas']);
+$Servidores=split('&',$_POST['Servidores']);
+$Retardos=split('&',$_POST['Retardos']);
+$Jitters=split('&',$_POST['Jitters']);
 
 for ($i = 1; $i < $Longitud ; $i++) {
-    
-    echo $i;
+
+$consulta=mysql_query("INSERT INTO ping (DELAY,JITTER,SERVER,DATETIME) VALUES('$Retardos[$i]','$Jitters[$i]','$Servidores[$i]','$Fechas[$i]')");
+mysql_free_result($consulta);
 }
 
-
-//$consulta=mysql_query("INSERT INTO Pings (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO,FECHA_HORA_SERV) VALUES('$_POST[Latitud_gps]','$_POST[Longitud_gps]','$_POST[Fecha_Hora_gps]','$_POST[Id_vehiculo]','$fecha_servidor')");
-
-//mysql_free_result($consulta);
-//mysql_close($conexion);
+mysql_close($conexion);
+echo "Guardado Satisfactoriamente";
 
 ?>
