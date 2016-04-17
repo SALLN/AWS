faf7a5c439f64b0bd3eb24ca2a24e877b59a7694
SALLN <stevenllerena@hotmail.com>
2016-04-17 00:45:40 -0500

    1.6

--- Android_Ping.php
+++ Android_Ping.php
@@ -6,8 +6,18 @@ $Fechas=$_POST['Fechas'];
 $Servidores=$_POST['Servidores'];
 $Retardos=$_POST['Retardos'];
 $Jitters=$_POST['Jitters'];
-echo "\nSi responde";
-echo "\nTodo: ".$Jitters;
+
+
+$Fechas=split('&',$Fechas);
+//$Servidores=$_POST['Servidores'];
+//$Retardos=$_POST['Retardos'];
+//$Jitters=$_POST['Jitters'];
+
+echo "Si responde\n";
+echo $Fechas[0];
+echo $Fechas[1];
+
+
 
 //$consulta=mysql_query("INSERT INTO Pings (LATITUD,LONGITUD,FECHA_HORA,ID_VEHICULO,FECHA_HORA_SERV) VALUES('$_POST[Latitud_gps]','$_POST[Longitud_gps]','$_POST[Fecha_Hora_gps]','$_POST[Id_vehiculo]','$fecha_servidor')");
 
