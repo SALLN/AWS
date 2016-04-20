f1da60439054d7172e68fe0c7571e99481a07035
SALLN <stevenllerena@hotmail.com>
2016-04-19 19:11:31 -0500

    2.1

--- Android_Ping.php
+++ Android_Ping.php
@@ -3,18 +3,23 @@
 include("MySQL/ConexionMySQL.php");
 
 $Fechas=split('&',$_POST['Fechas']);
-$Servidores=split('&',$_POST['Servidores']);
+$Servidores=$_POST['Servidores'];
 $Retardos=split('&',$_POST['Retardos']);
 $Jitters=split('&',$_POST['Jitters']);
-
+$Tipo=$_POST['Tipo'];
+$Realizacion=$_POST['Realizacion'];
+//$ipOrigen=$_POST['ipOrigen'];
+/*
 $Longitud=count($Retardos);
 for ($i = 1; $i < $Longitud ; $i++) {
 
-$consulta=mysql_query("INSERT INTO ping (DELAY,JITTER,SERVER,DATETIME) VALUES('$Retardos[$i]','$Jitters[$i]','$Servidores[$i]','$Fechas[$i]')");
+$consulta=mysql_query("INSERT INTO ping (idRealizacion,tipoPrueba,fecha,ipOrigen,ipDestino,delay,jitter) VALUES('$Realizacion','$Tipo','$Fechas[$i]','$ipOrigen','$Servidores[$i]','$Retardos[$i]','$Jitters[$i]')");
 mysql_free_result($consulta);
 }
 
 mysql_close($conexion);
+*/
+echo $_POST['REMOTE_ADDR']."okk";
 echo "Guardado Satisfactoriamente";
 
 ?>
