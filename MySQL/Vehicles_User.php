6a5a4df93eb3c0aba84e5cfbf6c9afab3abc41a6
SALLN <stevenllerena@hotmail.com>
2016-07-22 20:48:29 -0500

    2

--- MySQL/Vehicles_User.php
+++ MySQL/Vehicles_User.php
@@ -3,21 +3,29 @@ session_start();
 include("ConexionMySQL.php");
 
 
+$tabla=array();
+$i=0;
+
 if ($_POST['App']=="APP")
 {
 $consulta=mysql_query("SELECT ID_VEHICULO FROM FamiliaLlerenaNavarro GROUP BY ID_VEHICULO") or die("Problemas en consulta: ".mysql_error());
-}
-else
-{
-$consulta=mysql_query("SELECT ID_VEHICULO FROM $_SESSION[user] GROUP BY ID_VEHICULO") or die("Problemas en consulta: ".mysql_error());
-}
-$tabla=array();
-$i=0;
-while($reg=mysql_fetch_array($consulta)){  
+    while($reg=mysql_fetch_array($consulta)){  
 
 	$tabla[$i]=$reg['ID_VEHICULO'];
 	$i++;
 }
+}
+else
+{
+$consulta=mysql_query("SELECT ID_VEHICULO FROM $_SESSION[user] GROUP BY ID_VEHICULO") or die("Problemas en consulta: ".mysql_error());
+    while($reg=mysql_fetch_array($consulta)){  
+
+	$tabla[$i]=$reg;
+	$i++;
+}
+}
+
+
 
 echo json_encode($tabla);
 
