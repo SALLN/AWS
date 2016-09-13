8c105f5b9fc0c0ea3a894bd7da1dc056ad76ee69
SALLN <stevenllerena@hotmail.com>
2016-09-13 16:24:54 -0500

    as

--- INICIAR_SESION/SaveUsers.php
+++ INICIAR_SESION/SaveUsers.php
@@ -8,15 +8,15 @@ $usuario = $_POST['userid'];
 $contrasena = $_POST['password'];
 
 
-$q=mysql_query("SELECT * FROM admin WHERE user='$_POST[userid]'");
+$q=mysqli_query($conexion,"SELECT * FROM admin WHERE user='$_POST[userid]'");
 
-if (isset($_POST['userid']) && !empty($_POST['userid']) && isset($_POST['password']) && !empty($_POST['password']) && (preg_match("/^[a-zA-Z0-9\-_]+$/",$usuario)) && 
+if (isset($_POST['userid']) && !empty($_POST['userid']) && isset($_POST['password']) && !empty($_POST['password']) && (preg_match("/^[a-zA-Z0-9\-_]+$/",$usuario)) &&
 (preg_match("/^[a-zA-Z0-9\-_]+$/",$contrasena)) && (mysql_num_rows($q)==0) ){
 
 
-$r=mysql_query("INSERT INTO admin(user,pw) VALUES ('$usuario','$contrasena')");
+$r=mysqli_query($conexion,"INSERT INTO admin(user,pw) VALUES ('$usuario','$contrasena')");
 
-$s=mysql_query("CREATE TABLE $usuario(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, LATITUD VARCHAR(20), LONGITUD VARCHAR(20), FECHA_HORA datetime,  ID_VEHICULO VARCHAR(20),FECHA_HORA_SERV datetime )");
+$s=mysqli_query($conexion,"CREATE TABLE $usuario(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, LATITUD VARCHAR(20), LONGITUD VARCHAR(20), FECHA_HORA datetime,  ID_VEHICULO VARCHAR(20),FECHA_HORA_SERV datetime )");
 
 echo "<script type='text/javascript'>";
 echo "alert('Felicitaciones, usted se ha registrado satisfactoriamente');";
@@ -31,48 +31,44 @@ echo "</script> ";
 if(  ($_POST['userid']=="") || ($_POST['password']=="")  ){
 
     echo "<script type='text/javascript'>";
-    echo "alert('No se permiten campos en blanco')"; 
+    echo "alert('No se permiten campos en blanco')";
     echo "</script> ";
 
     echo "<script type='text/javascript'>";
     echo "window.close();";
     echo "</script> ";
-   
+
 }
 
 if(  (mysql_num_rows($q)!=0) ){
 
     echo "<script type='text/javascript'>";
-    echo "alert('El nombre de usuario  ya existe')"; 
+    echo "alert('El nombre de usuario  ya existe')";
     echo "</script> ";
 
     echo "<script type='text/javascript'>";
     echo "window.close();";
     echo "</script> ";
-   
+
 
 }
 
 if(!preg_match("/^[a-zA-Z0-9\-_]+$/", $usuario)){
-	 
+
 
     echo "<script type='text/javascript'>";
-    echo "alert('Caracteres no permitidos')"; 
+    echo "alert('Caracteres no permitidos')";
     echo "</script> ";
 
     echo "<script type='text/javascript'>";
     echo "window.close();";
     echo "</script> ";
-   
+
 
  }
-             
-mysql_free_result($q);
-mysql_free_result($r);
-mysql_free_result($s);
-mysql_close($conexion);
+
+mysqli_free_result($q);
+mysqli_free_result($r);
+mysqli_free_result($s);
+mysqli_close($conexion);
 ?>
-
-
-
-
--- INICIAR_SESION/validar.php
+++ INICIAR_SESION/validar.php
@@ -1,5 +1,5 @@
 <?php
-	session_start(); 
+	session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,10 +15,10 @@
 			if(isset($_POST['login'])){
 				$usuario = $_POST['user'];
 				$pw = $_POST['pw'];
-				$consulta = mysql_query("SELECT * FROM admin WHERE user='$usuario' AND pw='$pw'");
-				if (mysql_num_rows($consulta)>0) {
-					$row = mysql_fetch_array($consulta);
-					$_SESSION["user"] = $row['user']; 
+				$consulta = mysqli_query($conexion,"SELECT * FROM admin WHERE user='$usuario' AND pw='$pw'");
+				if (mysqli_num_rows($consulta)>0) {
+					$row = mysqli_fetch_array($consulta);
+					$_SESSION["user"] = $row['user'];
 				  	echo 'Iniciando sesión para '.$_SESSION['user'].' <p>';
 					echo '<script> window.location="../mapa.php"; </script>';
 				}
@@ -27,8 +27,8 @@
 					echo '<script> window.location="inicio_sesion.php"; </script>';
 				}
 			}
-            mysql_free_result($consulta);
-            mysql_close($conexion);
-		?>	
+            mysqli_free_result($consulta);
+            mysqli_close($conexion);
+		?>
 </body>
 </html>
