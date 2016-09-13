98149cfcee41dd55e475d9465da96aa6fc382db6
SALLN <stevenllerena@hotmail.com>
2016-09-13 16:39:56 -0500

    fetch array

--- MySQL/Vehicles_User.php
+++ MySQL/Vehicles_User.php
@@ -9,7 +9,7 @@ $i=0;
 if ($_POST['App']=="APP")
 {
 $consulta=mysqli_query($conexion,"SELECT ID_VEHICULO FROM FamiliaLlerenaNavarro GROUP BY ID_VEHICULO");
-    while($reg=mysql_fetch_array($consulta)){
+    while($reg=mysqli_fetch_array($consulta,MYSQLI_ASSOC)){
 
 	$tabla[$i]=$reg['ID_VEHICULO'];
 	$i++;
@@ -18,7 +18,7 @@ $consulta=mysqli_query($conexion,"SELECT ID_VEHICULO FROM FamiliaLlerenaNavarro 
 else
 {
 $consulta=mysqli_query($conexion,"SELECT ID_VEHICULO FROM $_SESSION[user] GROUP BY ID_VEHICULO");
-    while($reg=mysql_fetch_array($consulta)){
+    while($reg=mysqli_fetch_array($consulta,MYSQLI_ASSOC)){
 
 	$tabla[$i]=$reg;
 	$i++;
@@ -29,8 +29,8 @@ $consulta=mysqli_query($conexion,"SELECT ID_VEHICULO FROM $_SESSION[user] GROUP 
 
 echo json_encode($tabla);
 
-mysql_free_result($consulta);
-mysql_close($conexion);
+mysqli_free_result($consulta);
+mysqli_close($conexion);
 
 
 ?>
